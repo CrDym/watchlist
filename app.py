965f6f14b076dfe8537e4a1ed646ed64d1f6716e
rock <739965647@qq.com>
2020-11-18 18:27:57 +0800

    add: index page

--- app.py
+++ app.py
@@ -4,11 +4,24 @@
 # Author： rockche
 # Date:  2020/11/18  5:02 下午
 # Description : 程序主页
-from flask import Flask
+from flask import Flask ,render_template
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
-    return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
+def index():
+    return render_template('index.html', name=name, movies=movies)
 
+name = 'CR'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
