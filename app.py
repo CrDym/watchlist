13d04aaedd32ce16eb2f3d81492c7ad6179a39aa
rock <739965647@qq.com>
2020-11-20 11:41:29 +0800

    feat: User authentication with Flask-Login

--- app.py
+++ app.py
@@ -11,6 +11,8 @@ import click
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask import request, url_for, redirect, flash
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, UserMixin, login_required, current_user, login_user, logout_user
 
 WIN = sys.platform.startswith('win')
 if WIN:
@@ -25,6 +27,15 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的
 # 在扩展实例化前加载配置
 db = SQLAlchemy(app)
 
+login_manager =LoginManager(app)
+
+@login_manager.user_loader
+def user_loader(user_id):
+    user = User.query.get(int(user_id))
+    return user
+
+
+login_manager.login_view = 'Login'
 
 @app.cli.command()  # 注册为命令
 @click.option('--drop', is_flag=True, help='Create after drop.')  # 设置选项
@@ -60,9 +71,17 @@ def forge():
     click.echo('Done.')
 
 
-class User(db.Model):  # 表名将会是user 自动生成 小写
+class User(db.Model,UserMixin):  # 表名将会是user 自动生成 小写
     id = db.Column(db.Integer, primary_key=True)  # 主键
     name = db.Column(db.String(20))  # 名字
+    username = db.Column(db.String(20))  # 用户名
+    password_hash = db.Column(db.String(128)) # 密码
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def validate_password(self,password):
+        return check_password_hash(self.password_hash,password)
 
 
 class Movie(db.Model):
@@ -95,6 +114,8 @@ def page_not_found(e):
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
+        if not current_user.is_authenticated:
+            return redirect(url_for('index'))
         title = request.form.get('title')
         year = request.form.get('year')
         # 验证数据
@@ -112,6 +133,7 @@ def index():
 
 
 @app.route('/movie/edit/<int:movie_id>', methods=['GET', 'POST'])
+@login_required  # 登录保护
 def edit(movie_id):
     movie = Movie.query.get_or_404(movie_id)
 
@@ -133,6 +155,7 @@ def edit(movie_id):
 
 
 @app.route('/movie/delete/<int:movie_id>', methods=['POST'])  # 限定只接受 POST 请求
+@login_required  # 登录保护
 def delete(movie_id):
     movie = Movie.query.get_or_404(movie_id)  # 获取电影记录
     db.session.delete(movie)  # 删除对应的记录
@@ -140,4 +163,75 @@ def delete(movie_id):
     flash('Item deleted.')
     return redirect(url_for('index'))  # 重定向回主页
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
 
+        if not username or not password:
+            flash('Invalid input.')
+            return redirect(url_for('login'))
+
+        user = User.query.first()
+        # 验证用户名和密码是否一致
+        if username == user.username and user.validate_password(password):
+            login_user(user)  # 登入用户
+            flash('Login success.')
+            return redirect(url_for('index'))  # 重定向到主页
+
+        flash('Invalid username or password.')  # 如果验证失败，显示错误消息
+        return redirect(url_for('login'))  # 重定向回登录页面
+
+    return render_template('login.html')
+
+@app.route('/logout')
+@login_required  # 用于视图保护，后面会详细介绍
+def logout():
+    logout_user()  # 登出用户
+    flash('Goodbye.')
+    return redirect(url_for('index'))  # 重定向回首页
+
+
+@app.route('/settings', methods=['GET', 'POST'])
+@login_required
+def settings():
+    if request.method == 'POST':
+        name = request.form['name']
+
+        if not name or len(name) > 20:
+            flash('Invalid input.')
+            return redirect(url_for('settings'))
+
+        current_user.name = name
+        # current_user 会返回当前登录用户的数据库记录对象
+        # 等同于下面的用法
+        # user = User.query.first()
+        # user.name = name
+        db.session.commit()
+        flash('Settings updated.')
+        return redirect(url_for('index'))
+
+    return render_template('settings.html')
+
+
+@app.cli.command()
+@click.option('--username',prompt=True, help='The Username used to login.')
+@click.option('--password',prompt = True, hide_input=True,confirmation_prompt=True,help='The Password used to login.')
+def admin(username, password):
+    '''Create user.'''
+    db.create_all()
+
+    user = User.query.first()
+    if user is not None:
+        click.echo('Updating user...')
+        user.username = username
+        user.set_password(password)
+    else:
+        click.echo('Creating user...')
+        user = User(username=username, name='Admin')
+        user.set_password(password)
+        db.session.add(user)
+
+    db.session.commit()
+    click.echo('Done.')
