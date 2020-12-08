09d26e38bf53c3d5cc7acde41afa9d9dfba23a03
rock <739965647@qq.com>
2020-12-08 17:45:09 +0800

    Organize application with package

--- test_watchlist.py
+++ test_watchlist.py
@@ -5,7 +5,9 @@
 # Date:  2020/11/21  1:58 下午
 # Description :
 import unittest
-from app import app, db, Movie, User, forge, initdb
+from watchlist import app, db
+from watchlist.models import Movie, User
+from watchlist.commands import forge, initdb
 
 
 class WatchlistTestCase(unittest.TestCase):
